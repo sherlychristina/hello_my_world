7d2491bfa5911de15c2281eac7d476242e20c638
Sherly Christina <sherly.christina.upr@gmail.com>
2021-03-31 14:21:14 +0700

    Edit loop di C++

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
 
 #include <iostream>
 
+int queue[100];
 int main()
 {
     std::cout << "Hello, world!\n";
+    std::cout << "Hello, Don!\n";
+    std::cout << "Hello, Sam!\n";
+    for (int i = front; i <= rear; i++)
+      std::cout<<queue[i]<<" ";
+
 }
