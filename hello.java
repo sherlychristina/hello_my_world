f403da7b6c367f5e994cb2f063b5f62cba243143
Sherly Christina <sherly.christina.upr@gmail.com>
2021-03-31 12:05:39 +0700

    Adding initial files

---
+++ hello.java
@@ -0,0 +1,13 @@
+//'main' method must be in a class 'Rextester'.
+//openjdk version '11.0.5' 
+
+import java.util.*;
+import java.lang.*;
+
+class Rextester
+{  
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!");
+    }
+}
